1993a86d06d7701c9b63ca9ada704e064a9d6ed1
acool8868 <acool8868@gmail.com>
2025-09-01 01:07:56 +0530

    supports n randon trading agents

--- OrderBook/agents.h
+++ OrderBook/agents.h
@@ -3,8 +3,79 @@
 //  OrderBook
 //
 
-class Agents {
+class Agent {
 private:
-    
+    int agentID;
+    Gateway& gateway;
+    std::thread agent_thread;
+    std::atomic<bool>& flag;
+public:
+    Agent(int agentID, Gateway& gateway, std::atomic<bool>& flag)
+        :agentID(agentID),
+        gateway(gateway),
+        flag(flag)
+    {}
+
+    Agent(const Agent&) = delete;
+    Agent& operator=(const Agent&) = delete;
+
+
+    void start() {
+       agent_thread = std::thread(&Agent::run, this);
+    }
+
+    void join() {
+       if (agent_thread.joinable()) {
+           agent_thread.join();
+       }
+    }
+
+private:
+
+    void run() {
+        // Each thread needs its own random number generator
+        std::mt19937 rng(static_cast<unsigned>(agentID) + std::chrono::high_resolution_clock::now().time_since_epoch().count());
+        std::uniform_int_distribution<int> qty_dist(1, 20);
+        std::uniform_real_distribution<double> price_dist(90.0, 105.0);
+        std::uniform_real_distribution<double> price_dist2(95.0, 110.0);
+        
+        while (flag) {
+            // Generate random order details
+            //Price price = std::round(price_dist(rng) * 100.0/100);
+            Quantity quantity = qty_dist(rng);
+            Side side = (!(rng() % 3 == 0)) ? Side::Buy : Side::Sell;
+            Price price = (rng() % 3 == 0) ? std::round(price_dist(rng))*100 : std::round(price_dist2(rng))*100;
+            OrderID order_id = (static_cast<OrderID>(agentID) << 32) | std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+            auto order = std::make_unique<Order>(order_id, price, quantity, OrderType::Limit, side);
+            auto command = std::make_unique<Command>(order);
+            gateway.Push(std::move(command));
+            std::cout << "order_placed by " << agentID << " ";
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(rng() % 100 + 50));
+        }
+    }
 };
 
+class AgentManager {
+private:
+    std::vector<std::unique_ptr<Agent>> agents;
+public:
+    AgentManager(int numberOfAgents, Gateway& gateway, std::atomic<bool>& flag){
+        for(int i = 0; i<numberOfAgents; i++){
+            agents.emplace_back(std::make_unique<Agent>(i+1, gateway, flag));
+        }
+    }
+    
+    void StartAll(){
+        for(auto& agent: agents){
+            agent->start();
+        }
+    }
+    
+    void join_all() {
+        for (auto& agent : agents) {
+            agent->join();
+        }
+    }
+};
--- OrderBook/main.cpp
+++ OrderBook/main.cpp
@@ -8,88 +8,53 @@
 #include "exchange.h"
 #include "agents.h"
 #include "hft.h"
+#include "testdummyorders.h"
 
-
-void dummy_start_orders(Exchange* exchange, Gateway* gateway){
-    auto now = std::chrono::steady_clock::now().time_since_epoch();
-    std::default_random_engine rng(static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count()));
-    std::uniform_int_distribution<int> qty_dist(1, 20);
-    std::uniform_real_distribution<double> price_dist1(90.0, 110.0);
-    //std::uniform_real_distribution<double> price_dist2(95.0, 109.0);
-    int buys = 0;
-    int sells = 0;
-
-    for (int id = 1; id <= 10000; id++) {
-        double price = std::round(price_dist1(rng) * 100.0);
-        int qty = qty_dist(rng);
-        Side side = Side::Buy;
-        TimeStamp ts = now;
-
-        auto order = std::make_unique<Order>(id, price, qty, OrderType::Limit, side, ts);
-        auto command = std::make_unique<Command>(order);
-        //exchange->AddOrder(std::move(order));
-        std::cout<< "command pushed";
-        gateway->Push(std::move(command));
-        buys++;
-        //std::cout << "\033[2J\033[H" << std::flush;
-        //exchange->PrintBook();
-        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
-//        if ((qty)%2==0){
-//            continue;
-//        }
-        id++;
-        price = std::round(price_dist1(rng) * 100.0);
-        qty = qty_dist(rng);
-        side = Side::Sell;
-        ts = now;
-        order = std::make_unique<Order>(id, price, qty, OrderType::Limit, side, ts);
-        command = std::make_unique<Command>(order);
-        //exchange->AddOrder(std::move(order));
-        gateway->Push(std::move(command));
-        sells++;
-        //std::cout << "\033[2J\033[H" << std::flush;
-        //exchange->PrintBook();
-        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-    std::cout << " buys = "<< buys << "\nsells = " << sells;
-}
 
 int main(int argc, const char * argv[]) {
     Gateway gateway;
     OrderManager orderManager;
     OrderBook orderBook;
     Exchange exchange(orderBook, orderManager);
-    std::thread agents_thread(dummy_start_orders, &exchange, &gateway);
-    std::cout << "thread started";
-
+    
+    std::atomic<bool> flag(true);
+    const int numberOfAgents = 10;
+    AgentManager agentManager(numberOfAgents, gateway, flag);
+    std::cout << "Starting " << numberOfAgents << " concurrent agents..." << std::endl;
+    agentManager.StartAll();
+    
+    
+    //std::thread agents_thread(dummy_start_orders, &exchange, &gateway);
     //dummy_start_orders(exchangeptr, gatewayptr);
+    
     CommandPtr command;
-    while (agents_thread.joinable()){
-        
-        
-        
-        while (true){
-            gateway.WaitAndPop(command);
-            switch (command->type){
-                case CommandType::PlaceOrder:
-                    exchange.AddOrder(std::move(std::get<OrderPtr>(command->payload)));
-                    std::cout << "order added";
-                    break;
+    int i = 0;
+    while (i<1000){
+        gateway.WaitAndPop(command);
+        switch (command->type){
+            case CommandType::PlaceOrder:
+                exchange.AddOrder(std::move(std::get<OrderPtr>(command->payload)));
+                std::cout << "order_added ";
+                break;
 //                case CommandType::ModifyOrder:
 //                    orderManager.ModifyOrder(std::move(std::get<OrderPtr>(command->payload)));
 //                    Not implemented yet, skip this type
 //                    break;
-                case CommandType::CancelOrder:
-                    exchange.CancelOrder(std::get<OrderID>(command->payload));
-                    break;
-            }
-            //std::this_thread::sleep_for(std::chrono::microseconds(100));
+            case CommandType::CancelOrder:
+                exchange.CancelOrder(std::get<OrderID>(command->payload));
+                break;
         }
+        std::cout << "\033[2J\033[H" << std::flush;
+        orderBook.PrintOrderBook();
+        i++;
     }
+    
+    flag = false;
+    agentManager.join_all();
 
-    orderBook.PrintOrderBook();
-
-    return 0;
+//    orderBook.PrintOrderBook();
+//
+//    return 0;
 
 }
 
