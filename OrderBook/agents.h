6bfe07443523c1f64bb1d4390e41a05f5f737570
acool8868 <acool8868@gmail.com>
2025-09-05 01:02:52 +0530

    changed random distribution

--- OrderBook/agents.h
+++ OrderBook/agents.h
@@ -37,21 +37,20 @@ private:
         // Each thread needs its own random number generator
         std::mt19937 rng(static_cast<unsigned>(agentID) + std::chrono::high_resolution_clock::now().time_since_epoch().count());
         std::uniform_int_distribution<int> qty_dist(1, 20);
-        std::uniform_real_distribution<double> price_dist(90.0, 105.0);
-        std::uniform_real_distribution<double> price_dist2(95.0, 110.0);
+        std::normal_distribution<double> price_dist1(99, 1.0);
+        std::normal_distribution<double> price_dist2(101, 1.0);
+
         
         while (flag) {
             // Generate random order details
-            //Price price = std::round(price_dist(rng) * 100.0/100);
             Quantity quantity = qty_dist(rng);
             Side side = (!(rng() % 2 == 0)) ? Side::Buy : Side::Sell;
-            Price price = (rng() % 2 == 0) ? std::round(price_dist(rng))*100 : std::round(price_dist2(rng))*100;
+            Price price = (rng() % 2 == 0) ? price_dist1(rng)*100.0 : price_dist2(rng)*100.0;
             OrderID order_id = (static_cast<OrderID>(agentID) << 56)| counter;
             counter++;
             auto order = std::make_unique<Order>(order_id, price, quantity, OrderType::Limit, side);
             auto command = std::make_unique<Command>(order);
             gateway.Push(std::move(command));
-            //std::cout << "order_placed by " << agentID << " ";
 
             std::this_thread::sleep_for(std::chrono::milliseconds(rng() % 100 + 50));
         }
