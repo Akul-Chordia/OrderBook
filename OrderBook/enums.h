cf8838efb1b73a1215d643a9636ca1a1b36b0960
acool8868 <acool8868@gmail.com>
2025-09-05 01:05:01 +0530

    added spot price

--- OrderBook/enums.h
+++ OrderBook/enums.h
@@ -39,6 +39,7 @@ using Price = std::int32_t;  // 1 -> $0.01    (10132 -> $101.32)
 using Quantity = std::uint32_t;
 using OrderID = std::uint64_t;
 using TimeStamp = std::chrono::nanoseconds;
-
+using SpotPrice = std::int32_t;
+SpotPrice spotPrice= 100;
 constexpr std::int32_t INVALID = std::numeric_limits<std::int32_t>::min();
 
