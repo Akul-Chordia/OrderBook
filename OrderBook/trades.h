9605af64478a78ca7c2cb6975e2c4de99ba4475d
acool8868 <acool8868@gmail.com>
2025-09-05 01:03:33 +0530

    edits

--- OrderBook/trades.h
+++ OrderBook/trades.h
@@ -4,27 +4,36 @@
 //
 class Trade{
 private:
-    OrderID buySideOrderID;
-    OrderID sellSideOrderID;
+    OrderID agressorOrderID;
+    OrderID sittingOrderID;
+    Side side;
     Price price;
     Quantity quantity;
     TimeStamp timestamp;
     
 public:
-    Trade(OrderID buySideOrderID, OrderID sellSideOrderID, Price price, Quantity quantity, TimeStamp timestamp)
-    :buySideOrderID{buySideOrderID},
-    sellSideOrderID{sellSideOrderID},
+    Trade(OrderID agressorOrderID, OrderID sittingOrderID, Side side, Price price, Quantity quantity, TimeStamp timestamp = std::chrono::steady_clock::now().time_since_epoch())
+    :agressorOrderID{agressorOrderID},
+    sittingOrderID{sittingOrderID},
+    side{side},
     price{price},
     quantity{quantity},
     timestamp{timestamp}
-    {};
+    {
+        spotPrice = price;
+    };
     
     void Print() const {
-        std::cout << quantity << "@" << price << " on " << timestamp << " ( Buy order ID:" << buySideOrderID << " Sell order ID:" << sellSideOrderID << ")\n";
+        if (side == Side::Buy){
+            std::cout << agressorOrderID << " bought " << quantity << "@" << price << " on " << timestamp << " from " << sittingOrderID << "\n";
+        } else {
+            std::cout << agressorOrderID << " sold " << quantity << "@" << price << " on " << timestamp << " to " << sittingOrderID << "\n";
+        }
     }
 };
 
 using Trades = std::vector<Trade>;
+Trades trades;
 
 void print_trades(const Trades& trades){
     for (const auto& trade : trades) {
